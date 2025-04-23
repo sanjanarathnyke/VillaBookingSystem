1e52e1003b781e9c76293e074db87c2a9a3b3c50
sanjana <sanjanarathnayake692@gmail.com>
2025-04-23 16:28:20 +0530

    implement the cards to the dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function showDashboard()
     {
-        return view('Admin.dashboard');
+        $chartData = [
+            'countries' => DB::table('countries')
+                ->select('name', 'capital', 'area', 'currency')
+                ->get(),
+
+            'villadetails' => DB::table('villas')
+                ->select('name', 'price')
+                ->where('price', '>=', 2000)
+                ->get(),
+            'bookings' => DB::table('bookings')
+                ->select('extra_guests', 'checkin', 'checkout', 'total_price')
+                ->get(),
+        ];
+
+        
+        return view('Admin.dashboard', compact('chartData'));
     }
     public function showVillaBookings()
     {
