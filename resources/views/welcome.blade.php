bbad03cb80f48e959328040168e87da04ce636db
sanjana <sanjanarathnayake692@gmail.com>
2025-05-20 21:51:14 +0530

    implement the chatbot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -622,5 +622,10 @@
             <h3 class="cta-two__title">Your Perfect Escape: A Home Away from Home with World-Class Amenities</h3>
         </div>
     </div><!-- /.container -->
+    <section>
+        <script>
+            (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="ZrbAWSnjwtGsnzrx5lXHI";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+        </script>
+    </section>
 </section><!-- /.cta-one -->
 @endsection
